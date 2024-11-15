db76ffedccd7dd1806d645d8e74e2930aa6cdc04
Denxtov <denxtov@bk.ru>
2024-11-15 18:19:23 +0300

    Initial

--- module_6_3.py
+++ module_6_3.py
@@ -1,12 +1,52 @@
+from random import random
+
+
 class Animal():
     live=True
     sound=None
     _DEGREE_OF_DANGER = 0
-    def __init__(self,name:str,speed:int):
-        self.name=name
+    def __init__(self,speed:int):
         self.speed=speed
         self.coords=[0,0,0]
     def move(self,dx,dy,dz):
-        if self.coords[2]+dz*speed<0
+        if self.coords[2]+dz*self.speed<0:
             print("It's too deep, i can't dive :(")
-        self.coords=[dx,dy,dz]
+        self.coords+=[x*self.speed for x in self.coords]
+    def get_cords(self):
+        print(f'X={self.coords[0]} Y={self.coords[1]} Z={self.coords[2]}')
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER<5:
+            print("Sorry, i'm peaceful :)")
+        else: print("Be careful, i'm attacking you 0_0")
+    def speak(self):
+        print(self.sound)
+
+class Bird(Animal):
+    beak = True
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+    def dive_in(self,dz):
+        self.coords[2]-=abs(dz)*self.speed*0.5
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+class Duckbill(PoisonousAnimal,AquaticAnimal,Bird):
+    sound="Click-click-click"
+    def lay_eggs(self):
+        eggs_count=int(random()*4)+1
+        print(f'Here are(is){eggs_count} eggs for u')
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+
+db.lay_eggs()
+
