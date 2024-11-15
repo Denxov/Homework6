8a896197359b6467738a555f79f353d3cc1e4415
Denxtov <denxtov@bk.ru>
2024-11-15 18:34:38 +0300

    Initial

--- module_6_3.py
+++ module_6_3.py
@@ -11,10 +11,12 @@ class Animal():
     def move(self,dx,dy,dz):
         if self.coords[2]+dz*self.speed<0:
             print("It's too deep, i can't dive :(")
-        self.coords+=[x*self.speed for x in self.coords]
+        self.coords[0]+=dx*self.speed
+        self.coords[1]+=dy*self.speed
+        self.coords[2]+=dz*self.speed
+
     def get_cords(self):
         print(f'X={self.coords[0]} Y={self.coords[1]} Z={self.coords[2]}')
-
     def attack(self):
         if self._DEGREE_OF_DANGER<5:
             print("Sorry, i'm peaceful :)")
@@ -24,17 +26,21 @@ class Animal():
 
 class Bird(Animal):
     beak = True
+
 class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
     def dive_in(self,dz):
         self.coords[2]-=abs(dz)*self.speed*0.5
+
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
+
 class Duckbill(PoisonousAnimal,AquaticAnimal,Bird):
     sound="Click-click-click"
     def lay_eggs(self):
         eggs_count=int(random()*4)+1
         print(f'Here are(is){eggs_count} eggs for u')
+
 db = Duckbill(10)
 
 print(db.live)
